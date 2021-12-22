2de5912c4241b52fb77abdd497ff5a56d64012a1
minmay <minmay@localhost.localdomain>
2021-12-21 23:19:23 -0500

    Added support for non-Windows platforms

--- stream_data_manager.py
+++ stream_data_manager.py
@@ -1,9 +1,15 @@
 import os
+import platform
 import mysql.connector as msc
 from time import sleep
 from threading import Thread
 
+OS = platform.system()
+
+bksp = '\\' if OS == 'Windows' else '/'
+
 home = os.getcwd()
+
 
 stage1_flag = 0
 stage2_flag = 0
@@ -11,12 +17,12 @@ stage2_flag = 0
 dirs = ['processing', 'queue', 'processed']
 
 for dir in dirs:
-    os.mkdir(home + '\\' + dir)
+    os.mkdir(home + bksp + dir)
     
 mydb = msc.connect(
     host = "localhost",
-    user = "minmay",
-    password = "password"
+    user = "upadhyay",
+    password = "wordpass"
     )
 
 
@@ -35,7 +41,7 @@ class file_creator(Thread):
         global stage1_flag
         for file in range(100):
             name ='file_'+str(file)+'.txt'
-            with open(home + '\\' + dirs[0] + '\\' + name,'w') as newfile:
+            with open(home + bksp + dirs[0] + bksp + name,'w') as newfile:
                 pass
             newfile.close()
             cursor1 = mydb.cursor()
@@ -52,11 +58,11 @@ class stage1_move(Thread):
         global stage1_flag
         global stage2_flag
         while stage1_flag == 0:
-            queuef_list = os.listdir(home + '\\' + dirs[1])
-            processingf_list = os.listdir(home + '\\' + dirs[0])
+            queuef_list = os.listdir(home + bksp + dirs[1])
+            processingf_list = os.listdir(home + bksp + dirs[0])
             if len(queuef_list) == 0:
                 for file in processingf_list:
-                    os.replace(home + '\\' + dirs[0] + '\\' + file, home + '\\' + dirs[1] + '\\' + file )
+                    os.replace(home + bksp + dirs[0] + bksp + file, home + bksp + dirs[1] + bksp + file )
                 sleep(5)
         stage2_flag = 1
 
@@ -64,9 +70,9 @@ class stage2_move(Thread):
     def run(self):
         global stage2_flag
         while stage2_flag == 0:
-            queuef_list = os.listdir(home + '\\' + dirs[1])
+            queuef_list = os.listdir(home + bksp + dirs[1])
             for file in queuef_list:
-                os.replace(home + '\\' + dirs[1] + '\\' + file, home + '\\' + dirs[2] + '\\' + file )
+                os.replace(home + bksp + dirs[1] + bksp + file, home + bksp + dirs[2] + bksp + file )
                 cursor2 = mydb.cursor()
                 cursor2.execute("USE proc_db")
                 cursor2.execute ("""
